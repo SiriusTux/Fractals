dbe27bf8e08e4ee1d9e639f83574da8ff94782f7
Stefano Comuzzi <sirius.tux@gmail.com>
2018-02-22 05:37:15 +0100

    mandelbrot

---
+++ color.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random
+
+def initialyzeimage(x_p, y_p):
+	image = []
+	for j in range(y_p):
+		x_colors = []
+		for i in range(x_p):
+			x_colors.append(0)
+		image.append(x_colors)
+	return image
+
+def color_points():
+	x_p = 50
+	y_p = 50
+	image = initialyzeimage(x_p, y_p)
+	for j in range(y_p):
+		for i in range(x_p):
+			image[i][j] = random.randint(0,10)
+	plt.imshow(	image, origin='lower', extent=(0,5,0,5),
+				cmap=cm.nipy_spectral_r, interpolation='nearest')
+	plt.colorbar()
+	plt.show()
+
+if __name__ == '__main__':
+	color_points()
---
+++ mandelbrot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+from color import initialyzeimage
+
+def initialyzeplane(n):
+	ans = []
+	x_vect = np.linspace(-2.5, 1.0, num=n)
+	y_vect = np.linspace(-1.0, 1.0, num=n)
+	for y in y_vect:
+		row = []
+		for x in x_vect:
+			row.append((x,y))
+		ans.append(row)
+	return ans
+
+def mandelbrot(plane):
+	max_iteration = 1000
+	image = initialyzeimage(len(plane), len(plane))
+	for i in range(len(plane)):
+		for k in range(len(plane[i])):
+			x, y = plane[i][k]
+			z = complex(0., 0.)
+			c = complex(x, y)
+			iteration = 0
+			while abs(z) < 2 and iteration < max_iteration:
+				z = z*z + c
+				iteration += 1
+			image[i][k] = iteration
+	return image
+
+
+if __name__ == '__main__':
+	n = 400
+	plane = initialyzeplane(n)
+	fig = mandelbrot(plane)
+	plt.imshow(	fig, extent=(-2.5,1.0,-1.0,1.0),
+				cmap=cm.nipy_spectral_r, interpolation='nearest')
+	plt.colorbar()
+	plt.show()
+
+
