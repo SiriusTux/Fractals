a519037405c93aaa17bc898a01d9cbf3e0c930d6
Stefano Comuzzi <sirius.tux@gmail.com>
2018-03-01 05:45:52 +0100

    ok_decorator

--- decorator.py
+++ decorator.py
@@ -11,13 +11,3 @@ def timer(func):
 		print('Elapsed time: {:.2} sec'.format(end - start))
 		return ans
 	return f
-
-'''def initialyzeimage(x_p, y_p):
-	## Gives back a xp x yp zero matrix 
-	image = []
-	for j in range(y_p):
-		x_colors = []
-		for i in range(x_p):
-			x_colors.append(0)
-		image.append(x_colors)
-	return image'''
